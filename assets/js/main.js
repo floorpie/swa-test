fa027c15dc8f386a94390ce0d813614de1d6da3c
Paul D <pd@pd.com>
2025-05-28 17:42:08 +0100

    Updated profile - Unravel 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,8 +11,7 @@
 		$wrapper = $('#wrapper'),
 		$header = $('#header'),
 		$nav = $('#nav'),
-		$main = $('#main'),
-		$navPanelToggle, $navPanel, $navPanelInner;
+		$main = $('#main');
 
 	// Breakpoints.
 		breakpoints({
@@ -123,79 +122,6 @@
 	// Background.
 		$wrapper._parallax(0.925);
 
-	// Nav Panel.
-
-		// Toggle.
-			$navPanelToggle = $(
-				'<a href="#navPanel" id="navPanelToggle">Menu</a>'
-			)
-				.appendTo($wrapper);
-
-			// Change toggle styling once we've scrolled past the header.
-				$header.scrollex({
-					bottom: '5vh',
-					enter: function() {
-						$navPanelToggle.removeClass('alt');
-					},
-					leave: function() {
-						$navPanelToggle.addClass('alt');
-					}
-				});
-
-		// Panel.
-			$navPanel = $(
-				'<div id="navPanel">' +
-					'<nav>' +
-					'</nav>' +
-					'<a href="#navPanel" class="close"></a>' +
-				'</div>'
-			)
-				.appendTo($body)
-				.panel({
-					delay: 500,
-					hideOnClick: true,
-					hideOnSwipe: true,
-					resetScroll: true,
-					resetForms: true,
-					side: 'right',
-					target: $body,
-					visibleClass: 'is-navPanel-visible'
-				});
-
-			// Get inner.
-				$navPanelInner = $navPanel.children('nav');
-
-			// Move nav content on breakpoint change.
-				var $navContent = $nav.children();
-
-				breakpoints.on('>medium', function() {
-
-					// NavPanel -> Nav.
-						$navContent.appendTo($nav);
-
-					// Flip icon classes.
-						$nav.find('.icons, .icon')
-							.removeClass('alt');
-
-				});
-
-				breakpoints.on('<=medium', function() {
-
-					// Nav -> NavPanel.
-						$navContent.appendTo($navPanelInner);
-
-					// Flip icon classes.
-						$navPanelInner.find('.icons, .icon')
-							.addClass('alt');
-
-				});
-
-			// Hack: Disable transitions on WP.
-				if (browser.os == 'wp'
-				&&	browser.osVersion < 10)
-					$navPanel
-						.css('transition', 'none');
-
 	// Intro.
 		var $intro = $('#intro');
 
@@ -251,20 +177,7 @@
 						}
 					});
 
-			});
-
-			// Ensure #nav is visible on mobile
-			$(document).ready(function() {
-				if ($(window).width() <= 980) {
-					$('#nav').show(); // Ensure nav is visible
-				}
-			});
-
-			// Function to toggle the mobile menu
-			function toggleMenu() {
-				const navMenu = document.querySelector('nav ul');
-				navMenu.classList.toggle('active');
-			}
+				});
 
 		}
 
